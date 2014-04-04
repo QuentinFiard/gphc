5fb538185f255c4178309c1400f4e730c4477e88
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-04-04 21:08:21 +0200

    ENH: coded unpaint_block function

--- cmdgen.py
+++ cmdgen.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def do_block(block):
+def paint_block(block):
     """"
     Generates commands for blocks (numpy array)
 
@@ -18,6 +18,23 @@ def do_block(block):
                       for C in xrange(m) if block[R, C]])
 
 
+def unpaint_block(block):
+    """"
+    Generates commands for blocks (numpy array)
+
+    Returns
+    -------
+    string
+    """
+
+    block = np.array(block)
+
+    n, m = block.shape
+
+    return "\n".join(["ERASECELL %i %i" % (R, C) for R in xrange(n)
+                      for C in xrange(m) if block[R, C]])
+
+
 def gencmd(blocks, K=None):
     """
     Generate commands for a set of blocks.
@@ -28,7 +45,7 @@ def gencmd(blocks, K=None):
 
     """
 
-    return "\n".join(map(do_block, blocks))
+    return "\n".join(map(unpaint_block, blocks))
 
 
 print gencmd([np.eye(10)])
