1c7bea55b1e55a5e8bd6d67cad0e5ebbb30883a8
dohmatob elvis dopgima <elvis.dohmatob@inria.fr>
2014-04-04 22:13:18 +0200

    erase(..)

--- painting/tools/commands.py
+++ painting/tools/commands.py
@@ -14,11 +14,16 @@ def do_block(block):
     return 'PAINSTSQ %i %i %i' % (R, C, S)
 
 
-def erase(i, j):
-    return "ERASECELL %i %i" % (i, j)
+def erase(cell):
+    return "ERASECELL %i %i" % cell
+
+
+def erase_all(cells):
+    return "\n".join(map(erase, cells))
+
 
 def paint_all(blocks):
     return "\n".join(map(do_block, blocks))
 
 print paint_all([[[0, 10], [101, 100]], [[0, 10], [100, 103]]])
-print erase(4, 3)
+print erase_all([(4, 3), (0, 1)])
