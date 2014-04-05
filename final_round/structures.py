0ced975886945554d514f452a8fc96ea4c7b79e7
Quentin Fiard <quentin.fiard@polytechnique.org>
2014-04-05 11:33:07 +0200

    Added structure definitions

---
+++ final_round/structures.py
@@ -0,0 +1,24 @@
+class Intersection(object):
+  """An intersection"""
+  def __init__(self, lat, long):
+    self.lat = lat
+    self.long = long
+
+
+class Road(object):
+  """A road"""
+  def __init__(self, left, right, cost, distance):
+    self.left = left
+    self.right = right
+    self.cost = cost
+    self.distance
+
+
+class Data(object):
+  """Problem data"""
+  def __init__(self, intersections, roads, max_time, start, num_cars):
+    self.intersections = intersections
+    self.roads = roads
+    self.max_time = max_time
+    self.start = start
+    self.num_cars = num_cars
