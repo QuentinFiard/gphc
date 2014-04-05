2d76e90b5cff7bfd2d2fd4d99bd91929c5e77247
Robin Ricard <ricard.robin@gmail.com>
2014-04-05 16:37:18 +0200

    More than 1 million

--- final_round/algorithms/best_local_path.py
+++ final_round/algorithms/best_local_path.py
@@ -15,12 +15,12 @@ def best_local_path(intersect_idx, roads, roads_o, remaining, radius=5):
   min_cost = sys.maxint
   max_distance = 0
   best_path = []
-  current_malus = 0
+  best_malus = 0
   nr = neighbor_roads(intersect_idx, roads)
+  malus_active = 0
   if len(nr) < 1:
-    current_malus = 1
     nr = neighbor_roads(intersect_idx, roads_o)
-  best_malus = current_malus
+    malus_active = 1
   for road in nr:
     try:
       roads[road.left].pop(road.right)
@@ -31,7 +31,6 @@ def best_local_path(intersect_idx, roads, roads_o, remaining, radius=5):
     except:
       pass
     [path, cost, distance, malus] = best_local_path(road.right, roads, roads_o, remaining, radius - 1)
-    malus += current_malus
     try:
       roads[road.left][road.right] = roads_o[road.left][road.right]
     except:
@@ -43,11 +42,11 @@ def best_local_path(intersect_idx, roads, roads_o, remaining, radius=5):
     path = [road.right] + path
     cost += road.cost
     distance += road.distance
-    if float(distance - malus * 100) / cost > float(max_distance) / min_cost and remaining > cost:
+    if float(distance) / (cost + malus) > float(max_distance) / min_cost and remaining > cost:
       best_path = path
       min_cost = cost
       max_distance = distance
-      best_malus = current_malus
+      best_malus = malus + malus_active * cost
 
   try:
     roads[intersect_idx].pop(best_path[0])
