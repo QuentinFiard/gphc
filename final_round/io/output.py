cce81da666daf1643462700e6d073bbc8b082579
Benjamin Riquet <benjamin@secbridge.com>
2014-04-05 11:50:00 +0200

    Ouput list of vehicles

--- final_round/io/output.py
+++ final_round/io/output.py
@@ -1,2 +1,14 @@
-def show(solution):
-  print solution
+def output(vehicle_paths, show=False):
+    solution = []
+    num_vehicles = len(vehicle_paths)
+    solution.append(str(num_vehicles))
+
+    for path in vehicle_paths:
+        solution.append(str(len(path)))
+        solution += map(str, path)
+
+    print('\n'.join(solution))
+
+
+if __name__ == '__main__':
+    output([[1,2,3], [4,5,6]])
