af0fc829ab2b34a614b6e430bd2eb2afaf763015
Quentin Fiard <quentin.fiard@polytechnique.org>
2014-04-05 15:59:51 +0200

    Algorithm update

--- final_round/algorithms/quentin.py
+++ final_round/algorithms/quentin.py
@@ -1,13 +1,42 @@
+from sets import Set
+import heapq
+
 from to_networkx import to_networkx
+from split_graph import geo_split as split_graph
 import networkx
 from networkx.algorithms.community.kernighan_lin import kernighan_lin_bisection
+from networkx.algorithms.shortest_paths.dense import floyd_warshall
+from networkx.algorithms.euler import is_eulerian
+from networkx.algorithms.shortest_paths.generic import shortest_path
+
+
+def compute_closest_in_subgraph(graph, subgraph, start, key="cost"):
+  visited = Set()
+  heap = [(0, start)]
+  while True:
+    cost, node = heapq.heappop(heap)
+    if subgraph.has_node(node):
+      return node
+    visited.add(node)
+    for neighbor, data in graph[node].iteritems():
+      if neighbor not in visited:
+        heapq.heappush(
+            heap, (cost + data[key], neighbor))
+
+def run_car_in_subgraph(graph, start, subgraph):
+  closest_node = compute_closest_in_subgraph(graph, subgraph, start)
+  path = shortest_path(graph, start, closest_node)
+  print path
+  return [start]
+
+
 
 
 def run(data):
   g = to_networkx(data)
-  undirected_g = g.to_undirected()
-  a11, a12 = kernighan_lin_bisection(undirected_g, max_iter=1)
-  print a11
+  subgraphs = split_graph(g, data.num_cars)
+  paths = []
+  for subgraph in subgraphs:
+    paths.append(run_car_in_subgraph(g, data.start, subgraph))
 
-  # Dummy solution for now.
-  return [[data.start] for i in xrange(data.num_cars)]
+  return paths
--- final_round/algorithms/split_graph.py
+++ final_round/algorithms/split_graph.py
@@ -1,12 +1,37 @@
 import math
+from sets import Set
 
-def get_num_split(n):
+def check_power_of_2(n):
   l = math.log(n, 2)
   if math.floor(l) != l:
     print "%d is not a power of 2" % n
     assert False
-  return l
 
 def simple_split(g, n):
-  num_split = get_num_split(n)
-  pass
+  check_power_of_2(n)
+  if n == 1:
+    return g
+  if n > 2:
+    [a, b] = simple_split(g, 2)
+    return simple_split(a, n / 2) + simple_split(b, n / 2)
+
+def geo_split(g, n):
+  check_power_of_2(n)
+  if n == 1:
+    return g
+  if n > 2:
+    [a, b] = geo_split(g, 2)
+    return geo_split(a, n / 2) + geo_split(b, n / 2)
+  nodes = g.nodes(data=True)
+  if (max(nodes, key=lambda pair: pair[1]["lat"])[1]["lat"] -
+      min(nodes, key=lambda pair: pair[1]["lat"])[1]["lat"] >
+      max(nodes, key=lambda pair: pair[1]["long"])[1]["long"] -
+      min(nodes, key=lambda pair: pair[1]["long"])[1]["long"]):
+    key = "lat"
+  else:
+    key = "long"
+  nodes.sort(key=lambda pair: pair[1][key])
+  nodes = map(lambda pair: pair[0], nodes)
+  a = g.subgraph(nodes[:len(nodes)/2])
+  b = g.subgraph(nodes[len(nodes)/2:])
+  return [a, b]
