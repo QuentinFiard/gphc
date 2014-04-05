f60da1232cbe179291603f60e89959d676d7af13
Robin Ricard <ricard.robin@gmail.com>
2014-04-05 12:13:37 +0200

    Opp road

--- final_round/algorithms/tools.py
+++ final_round/algorithms/tools.py
@@ -8,12 +8,18 @@ def to_dict(roads):
     d[road.left][road.right] = road
   return d
 
-def neighbor_roads(intersect_idx, roads):
-  if intersect_idx in roads:
+def neighbor_roads(intersect_idx, roads_dict):
+  if intersect_idx in roads_dict:
     list = []
-    neighbor_dict = roads[intersect_idx]
+    neighbor_dict = roads_dict[intersect_idx]
     for key in neighbor_dict:
       list.append(neighbor_dict[key])
     return neighbor_dict
   else:
     return []
+
+def opposite_road(road, roads_dict):
+  if road.right in roads_dict:
+    if road.left in roads_dict[road.right]:
+      return roads_dict[road.right][road.left]
+  return None
